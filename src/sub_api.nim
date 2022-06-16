ca009c13f2b7cbf994a42321ce11eba7361b2758
flicko0 <flickerdroid211@gmail.com>
2022-06-16 17:23:41 +0530

    echo debug

--- src/sub_api.nim
+++ src/sub_api.nim
@@ -4,7 +4,8 @@ import os
 import asyncdispatch
 import json
 import strutils
-
+for pair in os.envPairs():
+    echo pair
 echo "startup..."
 
 const TRUSTED = @["00"]
@@ -12,44 +13,44 @@ const TRUSTED = @["00"]
 let redisClient = waitFor(connectdb())
 
 func trustedCheck(token: string): bool =
-        if token in TRUSTED:
-                return true
-        else:
-                return false
+    if token in TRUSTED:
+        return true
+    else:
+        return false
 
 router subroute:
-        get "/":
-                resp "Hello, World! "
+    get "/":
+        resp "Hello, World! "
 
-        get "/getsub":
-                resp "sub"
+    get "/getsub":
+        resp "sub"
 
-        get "/getsub/@sub_id":
-                resp "sub : " & @"sub"
+    get "/getsub/@sub_id":
+        resp "sub : " & @"sub"
 
-        post "/postsub/@token":
-                if not trustedCheck(@"token"):
-                        resp "unauthorized"
-                var push = parseJson(@"payload")
-                var name = push["name"].getStr()
-                var desc = push["desc"].getStr()
-                var image = push["image"].getStr()
-                await postSub(redisClient, name, desc, image)
+    post "/postsub/@token":
+        if not trustedCheck(@"token"):
+            resp "unauthorized"
+        var push = parseJson(@"payload")
+        var name = push["name"].getStr()
+        var desc = push["desc"].getStr()
+        var image = push["image"].getStr()
+        await postSub(redisClient, name, desc, image)
 
-        delete "/getsub/@sub_id/@token":
-                if not trustedCheck(@"token"):
-                        resp "unauthorized"
-                resp "delete sub : " & @"sub"
+    delete "/getsub/@sub_id/@token":
+        if not trustedCheck(@"token"):
+            resp "unauthorized"
+        resp "delete sub : " & @"sub"
 
 
 proc main() {.async.} =
-        var app = initJester(
-          subroute,
-          settings = newSettings(
-            port = parseInt(os.getEnv("PORT")).PORT
-                )
+    var app = initJester(
+      subroute,
+      settings = newSettings(
+        port = parseInt(os.getEnv("PORT")).PORT
         )
-        app.serve()
+    )
+    app.serve()
 
 waitFor(main())
 
